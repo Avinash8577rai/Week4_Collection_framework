a03115fb309ac1b32e41d16b8c3a8bd4c4e514db
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:38:12 +0530

    [Avinash] add a program in which we have to msort the list  using of set collection itnerface

--- src/main/java/Day2/SetInterface/ConvertSetSortedList/SortedList.java
+++ src/main/java/Day2/SetInterface/ConvertSetSortedList/SortedList.java
@@ -1,4 +1,23 @@
 package Day2.SetInterface.ConvertSetSortedList;
 
+import java.util.*;
+
 public class SortedList {
+    public static List<Integer> convertToSortedList(Set<Integer> set) {
+        List<Integer> sortedList = new ArrayList<>(set);
+        Collections.sort(sortedList); // Sorting in ascending order
+        return sortedList;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> hashSet = new HashSet<>();
+        hashSet.add(5);
+        hashSet.add(3);
+        hashSet.add(9);
+        hashSet.add(1);
+
+        List<Integer> sortedList = convertToSortedList(hashSet);
+        System.out.println("Sorted List: " + sortedList); // Output: [1, 3, 5, 9]
+    }
+
 }
