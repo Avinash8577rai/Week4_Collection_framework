715e6e8b890b9c5b55aa079f01ede8480d1e6865
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 14:00:25 +0530

    [Avinash] add. a program to check the  test cases of the problem of the subsetcheck using set

--- src/Test/java/SetInterface/FindSubsets/SubsetCheckTest.java
+++ src/Test/java/SetInterface/FindSubsets/SubsetCheckTest.java
@@ -1,4 +1,17 @@
 package SetInterface.FindSubsets;
+import Day2.SetInterface.FindSubsets.SubsetCheck;
+import org.junit.jupiter.api.Test;
+import java.util.Set;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SubsetCheckTest {
+
+    @Test
+    void testSubset() {
+        Set<Integer> set1 = Set.of(2, 3);
+        Set<Integer> set2 = Set.of(1, 2, 3, 4);
+        assertTrue(SubsetCheck.isSubset(set1, set2));
+    }
+
 }
+
