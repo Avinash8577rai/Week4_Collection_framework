a4aee9fdb6eeea97ad963feae4d2cf21631235b8
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:33:18 +0530

    [Avinash] add a program in which we have to find the binary number using Queue collection itnerface

--- src/main/java/Day2/QueueInterface/GenerateBinaryNumbers/BinaryNumber.java
+++ src/main/java/Day2/QueueInterface/GenerateBinaryNumbers/BinaryNumber.java
@@ -1,4 +1,34 @@
 package Day2.QueueInterface.GenerateBinaryNumbers;
 
+import java.util.*;
+
 public class BinaryNumber {
+
+
+    public static String[] generateBinaryNumbers(int N) {
+        String[] result = new String[N];
+        Queue<String> queue = new LinkedList<>();
+
+        queue.add("1");
+
+        for (int i = 0; i < N; i++) {
+            String current = queue.remove();
+            result[i] = current;
+
+            queue.add(current + "0");
+            queue.add(current + "1");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        String[] binaryNumbers = generateBinaryNumbers(N);
+
+        System.out.print("Binary Numbers: ");
+        for (String num : binaryNumbers) {
+            System.out.print(num + " ");
+        }
+
+    }
 }
