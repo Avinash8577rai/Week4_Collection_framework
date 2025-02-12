553a1315164039a86c9aed113888f99464b1bded
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:41:14 +0530

    [Avinash] add a program in which we have to check the union and intersection of two set using of set collection itnerface

--- src/main/java/Day2/SetInterface/UnionandIntersectionofTwoSets/SetOperation.java
+++ src/main/java/Day2/SetInterface/UnionandIntersectionofTwoSets/SetOperation.java
@@ -1,4 +1,30 @@
 package Day2.SetInterface.UnionandIntersectionofTwoSets;
 
-public class SetOperation {
-}
+import java.util.Set;
+import java.util.HashSet;
+
+    public class SetOperation {
+
+        // Method to compute the union of two sets
+        public static <T> Set<T> union(Set<T> set1, Set<T> set2) {
+            Set<T> result = new HashSet<>(set1);
+            result.addAll(set2);
+            return result;
+        }
+
+        // Method to compute the intersection of two sets
+        public static <T> Set<T> intersection(Set<T> set1, Set<T> set2) {
+            Set<T> result = new HashSet<>(set1);
+            result.retainAll(set2);
+            return result;
+        }
+
+        public static void main(String[] args) {
+            Set<Integer> set1 = Set.of(1, 2, 3);
+            Set<Integer> set2 = Set.of(3, 4, 5);
+
+            System.out.println("Union: " + union(set1, set2));           // Output: [1, 2, 3, 4, 5]
+            System.out.println("Intersection: " + intersection(set1, set2)); // Output: [3]
+        }
+    }
+
