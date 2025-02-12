64dadfb03224ccb6d4760b82c4a897eb574e6cd8
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:27:23 +0530

    [Avinash] add a program in which we have to rotate the element  using list collection itnerface

--- src/main/java/Day2/LIstInterface/RotateElementsinList/RotateElements.java
+++ src/main/java/Day2/LIstInterface/RotateElementsinList/RotateElements.java
@@ -1,4 +1,23 @@
 package Day2.LIstInterface.RotateElementsinList;
+import java.util.*;
 
 public class RotateElements {
-}
+        public static <T> void rotateList(List<T> list, int positions) {
+            int size = list.size();
+            if (size == 0 || positions % size == 0) {
+                return;
+            }
+
+            positions = positions % size;
+            Collections.rotate(list, -positions);
+        }
+
+        public static void main(String[] args) {
+            List<Integer> inputList = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+            int rotateBy = 2;
+
+            System.out.println("Original List: " + inputList);
+            rotateList(inputList, rotateBy);
+            System.out.println("Rotated List: " + inputList);
+        }
+    }
