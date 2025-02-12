392c06e5bec743a5943166a3a8beb36ade3908fa
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:30:15 +0530

    [Avinash] add a program in which we have to invert the map using Map collection itnerface

--- src/main/java/Day2/MapInterface/InvertMap/InvertMap.java
+++ src/main/java/Day2/MapInterface/InvertMap/InvertMap.java
@@ -1,4 +1,31 @@
 package Day2.MapInterface.InvertMap;
-
+import java.util.*;
 public class InvertMap {
-}
+
+
+        public static <K, V> Map<V, List<K>> invertMap(Map<K, V> inputMap) {
+            Map<V, List<K>> invertedMap = new HashMap<>();
+
+            for (Map.Entry<K, V> entry : inputMap.entrySet()) {
+                V value = entry.getValue();
+                K key = entry.getKey();
+
+                // Add key to the list of inverted map
+                invertedMap.computeIfAbsent(value, k -> new ArrayList<>()).add(key);
+            }
+
+            return invertedMap;
+        }
+
+        public static void main(String[] args) {
+            Map<String, Integer> originalMap = new HashMap<>();
+            originalMap.put("A", 1);
+            originalMap.put("B", 2);
+            originalMap.put("C", 1);
+
+            Map<Integer, List<String>> invertedMap = invertMap(originalMap);
+
+            System.out.println("Inverted Map: " + invertedMap);
+        }
+    }
+
