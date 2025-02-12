1a17cfc006a35823557a80129076f3cad14f0224
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:51:42 +0530

    [Avinash] add. a program to check the test cases of the rotatelement to pass the test cases

--- src/Test/java/ListInterface/RotateElementsinList/RotateElementsTest.java
+++ src/Test/java/ListInterface/RotateElementsinList/RotateElementsTest.java
@@ -1,4 +1,24 @@
 package ListInterface.RotateElementsinList;
+import Day2.LIstInterface.RotateElementsinList.RotateElements;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class RotateElements {
-}
+    public class RotateElementsTest {
+
+        private <T> List<T> getRotatedList(List<T> input, int positions) {
+            List<T> copy = new ArrayList<>(input);
+            RotateElements.rotateList(copy, positions);
+            return copy;
+        }
+
+        @Test
+        void testRotateByTwo() {
+            List<Integer> input = Arrays.asList(10, 20, 30, 40, 50);
+            List<Integer> expected = Arrays.asList(30, 40, 50, 10, 20);
+            assertEquals(expected, getRotatedList(input, 2));
+        }
+
+
+    }
+
