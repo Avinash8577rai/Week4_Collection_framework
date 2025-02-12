36146d1730ccb288d632d8c9325a6d55675e0035
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:26:49 +0530

    [Avinash] add a program in which we have to reverse the element oif list  using list collection itnerface

--- src/main/java/Day2/LIstInterface/ReverseList/Main.java
+++ src/main/java/Day2/LIstInterface/ReverseList/Main.java
@@ -1,18 +1,18 @@
 package Day2.LIstInterface.ReverseList;
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        // Example with ArrayList
+
         List<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
         System.out.println("Original ArrayList: " + arrayList);
-        reverseList(arrayList);
+        ReverseList.reverseList(arrayList);
         System.out.println("Reversed ArrayList: " + arrayList);
 
-        // Example with LinkedList
+
         List<Integer> linkedList = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
-        System.out.println("\nOriginal LinkedList: " + linkedList);
-        reverseList(linkedList);
+        System.out.println("Original LinkedList: " + linkedList);
+        ReverseList.reverseList(linkedList);
         System.out.println("Reversed LinkedList: " + linkedList);
     }
 }
-}
+
--- src/main/java/Day2/LIstInterface/ReverseList/ReverseList.java
+++ src/main/java/Day2/LIstInterface/ReverseList/ReverseList.java
@@ -1,4 +1,16 @@
 package Day2.LIstInterface.ReverseList;
 
-public class Reverselist {
+import java.util.*;
+public class ReverseList {
+    public static <T> void  reverseList(List<T> list) {
+        int left = 0;
+        int right = list.size() - 1;
+        while (left < right) {
+            T temp = list.get(left);
+            list.set(left, list.get(right));
+            list.set(right, temp);
+            left++;
+            right--;
+        }
+    }
 }
