2dd553e01543701f9eca137d091da138be29d0ea
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:25:43 +0530

    [Avinash] add a program in which we have to find duplicate element and remove it  using list collection itnerface

--- src/main/java/Day2/LIstInterface/RemoveDuplicates/RemoveDuplicates.java
+++ src/main/java/Day2/LIstInterface/RemoveDuplicates/RemoveDuplicates.java
@@ -1,4 +1,24 @@
 package Day2.LIstInterface.RemoveDuplicates;
 
+import java.util.*;
+
 public class RemoveDuplicates {
+    public static <T> List<T> removeDuplicates(List<T> list) {
+        Set<T> seen = new HashSet<>();
+        List<T> result = new ArrayList<>();
+
+        for (T item : list) {
+            if (!seen.contains(item)) {
+                seen.add(item);
+                result.add(item);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> inputList = Arrays.asList(3, 1, 2, 2, 3, 4);
+        List<Integer> result = removeDuplicates(inputList);
+        System.out.println(result); // Output: [3, 1, 2, 4]
+    }
 }
