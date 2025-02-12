75db0806f1cd9a5e5365de49d85559085da90f91
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 14:34:05 +0530

    [Avinash] add. a program to check the  test cases of the problem of the two sets are equals using set

--- src/Test/java/SetInterface/TwoSetsAreEqual/SetEqualityCheckerTest.java
+++ src/Test/java/SetInterface/TwoSetsAreEqual/SetEqualityCheckerTest.java
@@ -1,4 +1,28 @@
 package SetInterface.TwoSetsAreEqual;
+import Day2.SetInterface.TwoSetsAreEqual.SetEqualityChecker;
+import org.junit.jupiter.api.Test;
+import java.util.Set;
+import java.util.HashSet;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SetEqualityCheckerTest {
+
+    @Test
+    void testEqualSets() {
+        Set<Integer> set1 = Set.of(1, 2, 3);
+        Set<Integer> set2 = Set.of(3, 2, 1);
+        assertTrue(SetEqualityChecker.areSetsEqual(set1, set2));
+    }
+
+
+    @Test
+    void testDifferentSizes() {
+        Set<Integer> set1 = Set.of(1, 2, 3);
+        Set<Integer> set2 = Set.of(1, 2);
+        assertFalse(SetEqualityChecker.areSetsEqual(set1, set2));
+    }
+
+
+
 }
+
