74457fe03988c2331e7137ade9553301d6eddf5d
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:53:41 +0530

    [Avinash] add. a program to check the  test cases of the problem find the highest key value

--- src/Test/java/MapInterface/FindKeyHighestValue/FindMaxKeyTest.java
+++ src/Test/java/MapInterface/FindKeyHighestValue/FindMaxKeyTest.java
@@ -1,4 +1,18 @@
 package MapInterface.FindKeyHighestValue;
+import Day2.MapInterface.FindKeyHighestValue.FindMaxKey;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FindMaxKeyTest {
+
+    @Test
+    void testFindMaxKey() {
+        Map<String, Integer> inputMap = new HashMap<>();
+        inputMap.put("A", 10);
+        inputMap.put("B", 20);
+        inputMap.put("C", 15);
+
+        assertEquals("B", FindMaxKey.findKeyWithMaxValue(inputMap));
+    }
 }
