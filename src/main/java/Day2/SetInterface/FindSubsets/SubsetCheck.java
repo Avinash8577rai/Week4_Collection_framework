b3179b85dc7a37c47c89d526109ecb321a107b33
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:38:50 +0530

    [Avinash] add a program in which we have to check the number and there subset  using of set collection itnerface

--- src/main/java/Day2/SetInterface/FindSubsets/SubsetCheck.java
+++ src/main/java/Day2/SetInterface/FindSubsets/SubsetCheck.java
@@ -1,4 +1,19 @@
 package Day2.SetInterface.FindSubsets;
 
+import java.util.*;
+
 public class SubsetCheck {
+
+    // Method to check if set1 is a subset of set2
+    public static <T> boolean isSubset(Set<T> set1, Set<T> set2) {
+        return set2.containsAll(set1);
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = Set.of(2, 3);
+        Set<Integer> set2 = Set.of(1, 2, 3, 4);
+
+        System.out.println("Is set1 a subset of set2? " + isSubset(set1, set2)); // Output: true
+    }
 }
+
