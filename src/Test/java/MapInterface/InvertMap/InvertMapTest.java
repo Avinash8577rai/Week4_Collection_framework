e2e5beedee7e33533b58a74104e6f8c437a40708
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:54:45 +0530

    [Avinash] add. a program to check the  test cases of the problem of thr invert the map

--- src/Test/java/MapInterface/InvertMap/InvertMapTest.java
+++ src/Test/java/MapInterface/InvertMap/InvertMapTest.java
@@ -1,4 +1,23 @@
 package MapInterface.InvertMap;
+import Day2.MapInterface.InvertMap.InvertMap;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class InvertMapTest {
+    public class InvertMapTest {
+
+        @Test
+        void testInvertMap() {
+            Map<String, Integer> inputMap = new HashMap<>();
+            inputMap.put("A", 1);
+            inputMap.put("B", 2);
+            inputMap.put("C", 1);
+
+            Map<Integer, List<String>> expectedOutput = new HashMap<>();
+            expectedOutput.put(1, Arrays.asList("A", "C"));
+            expectedOutput.put(2, Arrays.asList("B"));
+
+            assertEquals(expectedOutput, InvertMap.invertMap(inputMap));
+        }
+
 }
