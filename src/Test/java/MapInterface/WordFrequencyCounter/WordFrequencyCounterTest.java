6acc82338e130e05e4976b6ccbc9b9f7c83cdc78
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:55:42 +0530

    [Avinash] add. a program to check the  test cases of the problem of the  word frequency count

--- src/Test/java/MapInterface/WordFrequencyCounter/WordFrequencyCounterTest.java
+++ src/Test/java/MapInterface/WordFrequencyCounter/WordFrequencyCounterTest.java
@@ -1,4 +1,36 @@
 package MapInterface.WordFrequencyCounter;
+import Day2.MapInterface.WordFrequencyCounter.WordFrequencyCounter;
+import org.junit.jupiter.api.Test;
+
+import java.io.PrintWriter;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WordFrequencyCounterTest {
+
+    @Test
+    void testWordFrequency() {
+
+        String testFilePath = "test.txt";
+
+
+        try (PrintWriter writer = new PrintWriter(testFilePath)) {
+            writer.println("Hello world, hello Java!");
+            writer.println("Java is fun. Hello everyone.");
+        } catch (Exception e) {
+            fail("Failed to create test file.");
+        }
+
+
+        Map<String, Integer> result = WordFrequencyCounter.countWordFrequency(testFilePath);
+        assertEquals(3, result.get("hello"));
+        assertEquals(2, result.get("java"));
+        assertEquals(1, result.get("world"));
+        assertEquals(1, result.get("is"));
+        assertEquals(1, result.get("fun"));
+        assertEquals(1, result.get("everyone"));
+    }
 }
+
+
+
