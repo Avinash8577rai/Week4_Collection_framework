8ce0f6b5173c485943b902539596b38b7f34c2ac
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:59:57 +0530

    [Avinash] add. a program to check the  test cases of the problem of the sorted list using set

--- src/Test/java/SetInterface/ConvertSetSortedList/SortedListTest.java
+++ src/Test/java/SetInterface/ConvertSetSortedList/SortedListTest.java
@@ -1,5 +1,21 @@
 package SetInterface.ConvertSetSortedList;
+import Day2.SetInterface.ConvertSetSortedList.SortedList;
+import java.util.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class SortedList {
-    
-}
+        public class SortedListTest {
+
+            @Test
+            void testConvertToSortedList() {
+                Set<Integer> set = Set.of(5, 3, 9, 1);
+                List<Integer> expected = List.of(1, 3, 5, 9);
+                assertEquals(expected, SortedList.convertToSortedList(set));
+            }
+        }
+
+
+
+
+
+
