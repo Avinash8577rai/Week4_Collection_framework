0949455c29b2457723460725e8f7400180b3535b
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:58:23 +0530

    [Avinash] add. a program to check the  test cases of the problem of the reverse the queue using queue

--- src/Test/java/QueueInterface/ReverseQueue/ReverseQueueTest.java
+++ src/Test/java/QueueInterface/ReverseQueue/ReverseQueueTest.java
@@ -1,4 +1,19 @@
 package QueueInterface.ReverseQueue;
+import Day2.QueueInterface.ReverseQueue.ReverseQueue;
 
-public class ReverseQueueTest {
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+    public class ReverseQueueTest {
+
+        @Test
+        void testReverseQueue() {
+            Queue<Integer> queue = new LinkedList<>(List.of(10, 20, 30));
+            Queue<Integer> expected = new LinkedList<>(List.of(30, 20, 10));
+
+            ReverseQueue.reverseQueue(queue);
+            assertEquals(expected, queue);
+    }
+
 }
