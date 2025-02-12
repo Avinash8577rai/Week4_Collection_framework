f957f6390173a6e30198554ae9dd74b7aec74ff1
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:48:41 +0530

    [Avinash] add. a program to check the test case of the frequncy of elements and pass the testcases

--- src/Test/java/ListInterface/FindFrequencyofElements/FrequencyListTest.java
+++ src/Test/java/ListInterface/FindFrequencyofElements/FrequencyListTest.java
@@ -1,4 +1,22 @@
 package ListInterface.FindFrequencyofElements;
 
+import Day2.LIstInterface.FindFrequencyofElements.FrequencyList;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FrequencyListTest {
+
+    @Test
+    void testFrequencyCount() {
+        List<String> input = Arrays.asList("apple", "banana", "apple", "orange");
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("apple", 2);
+        expected.put("banana", 1);
+        expected.put("orange", 1);
+
+        assertEquals(expected, FrequencyList.countFrequency(input));
+    }
+
 }
