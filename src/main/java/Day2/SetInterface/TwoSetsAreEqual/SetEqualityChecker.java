e8621ed216024a9d8b2b656ed1664dfcf2437d39
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:40:15 +0530

    [Avinash] add a program in which we have to check the set are eaquals or not using of set collection itnerface

--- src/main/java/Day2/SetInterface/TwoSetsAreEqual/SetEqualityChecker.java
+++ src/main/java/Day2/SetInterface/TwoSetsAreEqual/SetEqualityChecker.java
@@ -1,4 +1,23 @@
 package Day2.SetInterface.TwoSetsAreEqual;
-
+import java.util.Set;
+import java.util.HashSet;
 public class SetEqualityChecker {
-}
+
+        public static <T> boolean areSetsEqual(Set<T> set1, Set<T> set2) {
+            return set1.equals(set2);
+        }
+
+        public static void main(String[] args) {
+            Set<Integer> set1 = new HashSet<>();
+            set1.add(1);
+            set1.add(2);
+            set1.add(3);
+
+            Set<Integer> set2 = new HashSet<>();
+            set2.add(3);
+            set2.add(2);
+            set2.add(1);
+
+            System.out.println("Are sets equal? " + areSetsEqual(set1, set2)); // Output: true
+        }
+    }
