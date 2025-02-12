ac434df1249fb3a6916ee463ebb25beb82e9b045
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-12 13:57:19 +0530

    [Avinash] add. a program to check the  test cases of the problem of the genertae the binary number using queue

--- src/Test/java/QueueInterface/GenerateBinaryNumbers/BinaryNumberTest.java
+++ src/Test/java/QueueInterface/GenerateBinaryNumbers/BinaryNumberTest.java
@@ -1,4 +1,13 @@
 package QueueInterface.GenerateBinaryNumbers;
+import Day2.QueueInterface.GenerateBinaryNumbers.BinaryNumber;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BinaryNumberTest {
+
+    @Test
+    void testGenerateBinaryNumbers() {
+        assertArrayEquals(new String[]{"1", "10", "11", "100", "101"}, BinaryNumber.generateBinaryNumbers(5));
+    }
+
 }
