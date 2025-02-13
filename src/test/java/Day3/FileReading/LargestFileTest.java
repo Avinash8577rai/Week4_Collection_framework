4e901066efaebecd10e8bfdb2e00008d55238154
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-13 13:41:26 +0530

    [Avinash] add a program in which we run the testcase of the problem of read a large file using bufferstream and input stream

--- src/test/java/Day3/FileReading/LargestFileTest.java
+++ src/test/java/Day3/FileReading/LargestFileTest.java
@@ -1,4 +1,37 @@
 package Day3.FileReading;
 
-public class LargestFileTest {
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+import java.nio.file.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LargeFileReaderTest {
+    private static final String TEST_FILE = "test_largefile.txt";
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Files.write(Paths.get(TEST_FILE),
+                ("This is a test line.\n" +
+                        "An error occurred in the system.\n" +
+                        "Another normal line.\n" +
+                        "Critical ERROR found in logs.\n").getBytes());
+    }
+
+    @Test
+    void testReadLargeFile() throws Exception {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        LargestFile.readLargeFile(TEST_FILE);
+
+        String output = outContent.toString();
+        assertTrue(output.contains("error") || output.contains("ERROR"));
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        Files.deleteIfExists(Paths.get(TEST_FILE));
+    }
 }
