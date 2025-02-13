a5d87bf24e5d05404f9ef579b79692d1b89b3a38
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-13 13:43:37 +0530

    [Avinash] add a program in which we run the testcase of the problem readinput of the user from the files using input stream and bufferstream

--- src/test/java/Day3/ReadInput/UserInfoToFileTest.java
+++ src/test/java/Day3/ReadInput/UserInfoToFileTest.java
@@ -1,4 +1,35 @@
 package Day3.ReadInput;
+import Day3.ReadInput.UserInfoToFile;
+import org.junit.jupiter.api.*;
+import java.io.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserInfoToFileTest {
+    private static final String TEST_FILE = "C:/Users/AVINASH KUMAR RAI/Desktop/capgemini training/Destination.txt";
+
+    @AfterEach
+    void cleanUp() {
+        new File(TEST_FILE).delete();
+    }
+
+    @Test
+    void testFileWrite() throws IOException {
+
+        String simulatedInput = "avinash\n22\njava\n";
+
+        InputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        System.setIn(inputStream);
+
+        UserInfoToFile.main(new String[]{});
+
+        File file = new File(TEST_FILE);
+        assertTrue(file.exists());
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(TEST_FILE))) {
+            assertEquals("Name: avinash", reader.readLine());
+            assertEquals("Age: 22", reader.readLine());
+            assertEquals("Favorite programming lanuage : java", reader.readLine());
+        }
+    }
 }
+
