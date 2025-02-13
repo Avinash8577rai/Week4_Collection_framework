0661c4caa052286c059bcc3ea918b6dddeb08d2d
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-13 13:42:48 +0530

    [Avinash] add a program in which we run the testcase of the problem of piped stream

--- src/test/java/Day3/PipedStream/PipeStreamTest.java
+++ src/test/java/Day3/PipedStream/PipeStreamTest.java
@@ -1,4 +1,41 @@
 package Day3.PipedStream;
 
-public class PipeStreamTest {
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PipedStreamCommunicationTest {
+    private PipedOutputStream pos;
+    private PipedInputStream pis;
+    private WriterThread writer;
+    private ReaderThread reader;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        pos = new PipedOutputStream();
+        pis = new PipedInputStream(pos);
+
+        writer = new WriterThread(pos);
+        reader = new ReaderThread(pis);
+    }
+
+    @Test
+    void testInterThreadCommunication() throws Exception {
+        writer.start();
+        reader.start();
+
+        writer.join();
+        reader.join();
+
+        assertNotNull(pis);
+        assertNotNull(pos);
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        pos.close();
+        pis.close();
+    }
 }
