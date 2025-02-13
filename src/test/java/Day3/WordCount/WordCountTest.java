1def9cb33f34e8d9e8e0b33737bacac0c3a2fae3
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-13 13:44:55 +0530

    [Avinash] add a program in which we run the testcase of the problem of word count in any files usimng inputsream and bufferstream

--- src/test/java/Day3/WordCount/WordCountTest.java
+++ src/test/java/Day3/WordCount/WordCountTest.java
@@ -1,4 +1,38 @@
 package Day3.WordCount;
 
-public class WordCountTest {
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+import java.nio.file.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordCountTest {
+    private static final String TEST_FILE = "test_wordcount.txt";
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Files.write(Paths.get(TEST_FILE),
+                ("Hello world! Hello everyone.\n" +
+                        "This is a test file. Hello world.\n" +
+                        "Java is great. Java is powerful. Java is everywhere.\n").getBytes());
+    }
+
+    @Test
+    void testWordCount() throws Exception {
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        WordCount.countWords(TEST_FILE);
+
+        String output = outContent.toString();
+        assertTrue(output.contains("Total Words"));
+        assertTrue(output.contains("hello: 3"));
+        assertTrue(output.contains("java: 3"));
+    }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        Files.deleteIfExists(Paths.get(TEST_FILE));
+    }
 }
