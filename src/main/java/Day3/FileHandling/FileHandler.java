840b075a6de2ab0d7b616f25da1513f47ab985a6
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-13 13:30:07 +0530

    [Avinash] add a program in which we have to hanle the files

--- src/main/java/Day3/FileHandling/FileHandler.java
+++ src/main/java/Day3/FileHandling/FileHandler.java
@@ -1,4 +1,40 @@
 package Day3.FileHandling;
 
+import java.io.*;
+
 public class FileHandler {
+
+    public static void copyFile(String sourceFile, String destinationFile) throws IOException {
+        File src = new File(sourceFile);
+        File dest = new File(destinationFile);
+
+
+        if (!src.exists()) {
+            throw new FileNotFoundException("Source file does not exist: " + sourceFile);
+        }
+
+        try (FileInputStream fis = new FileInputStream(src);
+             FileOutputStream fos = new FileOutputStream(dest)) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+
+            while ((bytesRead = fis.read(buffer)) != -1) {
+                fos.write(buffer, 0, bytesRead);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String sourcePath = "C:/Users/AVINASH KUMAR RAI/Desktop/capgemini training/Gunjan sir's session ;.txt";
+        String destinationPath =  "C:/Users/AVINASH KUMAR RAI/Desktop/capgemini training/Destination.txt";
+
+        try {
+            copyFile(sourcePath, destinationPath);
+            System.out.println("File copied successfully!");
+        } catch (IOException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
 }
