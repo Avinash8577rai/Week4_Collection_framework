1ec46e747de3503e1f8363f71092ee081fe3ecc5
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:42:52 +0530

    [Avinash] add. a program of test cases of the program of divion class of unchecked exception

--- src/test/java/Day4ExceptionHandeling/UncheckedException/DivisionClassTest.java
+++ src/test/java/Day4ExceptionHandeling/UncheckedException/DivisionClassTest.java
@@ -1,4 +1,17 @@
 package Day4ExceptionHandeling.UncheckedException;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class DivisionClassTest {
+    @Test
+    void testValidDivision(){
+        assertEquals(5.0, DivisionClass.divisbleNumber(10,2));
+    }
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            DivisionClass.divisbleNumber( 0, 0);
+        });
+        assertTrue(exception.getMessage().contains("Cannot divide by zero"));
+    }
 }
