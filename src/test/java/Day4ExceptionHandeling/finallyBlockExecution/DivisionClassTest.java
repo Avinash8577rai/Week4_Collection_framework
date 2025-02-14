47dea2ea740a08b6f3490562f1ce2fc6a1989fba
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:38:48 +0530

    [Avinash] add. a program of test cases of the program of the divison of any number and use finally exception

--- src/test/java/Day4ExceptionHandeling/finallyBlockExecution/DivisionClassTest.java
+++ src/test/java/Day4ExceptionHandeling/finallyBlockExecution/DivisionClassTest.java
@@ -1,4 +1,19 @@
 package Day4ExceptionHandeling.finallyBlockExecution;
+import static org.junit.jupiter.api.Assertions.*;
 
+import Day4ExceptionHandeling.UncheckedException.DivisionClass;
+import org.junit.jupiter.api.Test;
 public class DivisionClassTest {
+
+    @Test
+    void testValidDivision(){
+        assertEquals(5.0, Day4ExceptionHandeling.UncheckedException.DivisionClass.divisbleNumber(10,2));
+    }
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            DivisionClass.divisbleNumber( 0, 0);
+        });
+        assertTrue(exception.getMessage().contains("Cannot divide by zero"));
+    }
 }
