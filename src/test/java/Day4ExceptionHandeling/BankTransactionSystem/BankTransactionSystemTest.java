a4a5599584f1c3ccc32f3a0aad92e2c35fc76f7e
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:36:22 +0530

    [Avinash] add. a program of test cases of the bank transtion system

--- src/test/java/Day4ExceptionHandeling/BankTransactionSystem/BankTransactionSystemTest.java
+++ src/test/java/Day4ExceptionHandeling/BankTransactionSystem/BankTransactionSystemTest.java
@@ -1,4 +1,28 @@
 package Day4ExceptionHandeling.BankTransactionSystem;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class BankTransactionSystemTest {
+    @Test
+    void testValidWithdrwal() throws InsufficientBalanceException {
+        BankAccount account=new BankAccount(5000);
+        account.withdraw(2000);
+        assertEquals(3000, account.getBalance());
+    }
+    @Test
+    void tesInsufficientBalance(){
+        BankAccount account=new BankAccount(5000);
+        Exception exception= assertThrows(InsufficientBalanceException.class,()->{
+            account.withdraw(6000);
+        });
+        assertEquals("Insufficient Balance in account",exception.getMessage());
+    }
+    @Test
+    void testNegativeAmount(){
+        BankAccount account=new BankAccount(6000);
+        Exception exception=assertThrows(IllegalArgumentException.class,()->{
+            account.withdraw(-500);
+        });
+        assertEquals("Invalid! amount", exception.getMessage());
+    }
+
 }
