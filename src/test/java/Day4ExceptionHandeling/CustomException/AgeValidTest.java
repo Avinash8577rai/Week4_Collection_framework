390ad49df6405e3f546ee3b7dafabfb8154bfac6
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:37:30 +0530

    [Avinash] add. a program of test cases of the program valid age for vote

--- src/test/java/Day4ExceptionHandeling/CustomException/AgeValidTest.java
+++ src/test/java/Day4ExceptionHandeling/CustomException/AgeValidTest.java
@@ -1,4 +1,26 @@
 package Day4ExceptionHandeling.CustomException;
 
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import Day4ExceptionHandeling.CustomException.AgeValid;
+import Day4ExceptionHandeling.CustomException.InvalidAgeException;
+import org.junit.jupiter.api.Test;
 public class AgeValidTest {
+
+    @Test
+    void testValidAge(){
+        assertDoesNotThrow(()-> AgeValid.validAge(18));
+    }
+    @Test
+    void testValidOlderAge(){
+        assertDoesNotThrow(()-> AgeValid.validAge(25));
+    }
+    @Test
+    void testInvalidAge(){
+        Exception exception = assertThrows(InvalidAgeException.class, () -> {
+            AgeValid.validAge(16);
+        });
+        assertTrue(exception.getMessage().contains("Age must be 18 or above"));
+    }
 }
