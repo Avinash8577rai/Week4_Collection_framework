66fa63d4a662c4134d5ca9b2c1798b5c957b7552
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:29:59 +0530

    [Avinash] add. a program of in which we we perfrom the arry operation and use multiple catch blocks to catch differeet exception and handle that exception

--- src/main/java/Day4ExceptionHandeling/MultipleCatchBlocks/ArrayOperation.java
+++ src/main/java/Day4ExceptionHandeling/MultipleCatchBlocks/ArrayOperation.java
@@ -1,4 +1,38 @@
 package Day4ExceptionHandeling.MultipleCatchBlocks;
-
+import java.util.*;
 public class ArrayOperation {
+    public static void getValueIndex(int[] arr, int index){
+        try{
+            System.out.println("Value at index:" +index + " : " + arr[index]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Index invalid!");
+        }
+        catch(NullPointerException e){
+            System.out.println("Array is not initialized!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+            System.out.println("Enter the size of the array is:");
+            int size = sc.nextInt();
+
+            int []  arr =new int[size];
+            System.out.println("Enter " + size + " Elements of array");
+            for(int i=0; i<size; i++){
+                arr[i]= sc.nextInt();
+            }
+            System.out.println("Enter the index to retrive the value:");
+            int index= sc.nextInt();
+
+            getValueIndex(arr, index);
+        }
+        catch(Exception e){
+            System.out.println("Invalid input! please enter the valid input");
+
+        }
+        sc.close();
+    }
 }
