2f3dc75646474204b9ae73b4f0c4ac77313f3566
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:39:30 +0530

    [Avinash] add. a program of test cases of the program of the array opertaion and test all the answer

--- src/test/java/Day4ExceptionHandeling/MultipleCatchBlocks/ArrayOperationTest.java
+++ src/test/java/Day4ExceptionHandeling/MultipleCatchBlocks/ArrayOperationTest.java
@@ -1,4 +1,31 @@
 package Day4ExceptionHandeling.MultipleCatchBlocks;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class ArrayOperationTest {
+
+    @Test
+    void testValidIndex(){
+        int[] arr= {10,20,30,40,50};
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        ArrayOperation.getValueIndex(arr, 3);
+        assertEquals("Value at index:3 : 40", outContent.toString().trim());
+
+    }
+    @Test
+    void testInvalidIndex(){
+        int[] arr ={ 10,20,30,40,50};
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        ArrayOperation.getValueIndex(arr , 10);
+        assertEquals("Index invalid!", outContent.toString().trim());
+    }
+
 }
