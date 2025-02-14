bed49bb920c8b32412260407d73aed2a75305283
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:33:08 +0530

    [Avinash] add. a program of in which we use buffer reader to read file and if not then give exception we have to handle that exception

--- src/main/java/Day4ExceptionHandeling/tryWithResources/BufferReader.java
+++ src/main/java/Day4ExceptionHandeling/tryWithResources/BufferReader.java
@@ -1,4 +1,25 @@
 package Day4ExceptionHandeling.tryWithResources;
+import java.io.*;
 
 public class BufferReader {
+    public static String readFiles(String fileName){
+        try(BufferedReader br= new BufferedReader((new FileReader(fileName)))){
+            return br.readLine();
+        }
+        catch(IOException e){
+            System.out.println("Error reading files using try with Resource");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        String fileName= " ";
+        String firstLine = readFiles(fileName);
+
+        if(firstLine!= null){
+            System.out.println("First Line: "+firstLine);
+        }
+    }
+
+
 }
