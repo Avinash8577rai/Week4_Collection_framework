89c2cf3eed7170ebb1d89a9145bdb669fd6801ea
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:24:45 +0530

    [Avinash] add. a program of in which we read the file and raised the checked exception and handle the exception

--- src/main/java/Day4ExceptionHandeling/CheckedException/CheckedException.java
+++ src/main/java/Day4ExceptionHandeling/CheckedException/CheckedException.java
@@ -1,4 +1,26 @@
 package Day4ExceptionHandeling.CheckedException;
-
+import java.io.*;
 public class CheckedException {
+    public static String readfiles(String fileName) {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("");
+
+            }
+        } catch (IOException e) {
+            System.out.println(" Error file not found " + e);
+        }
+        return content.toString().trim();
+    }
+
+    public static void main(String[] args) {
+        String result = readfiles("");
+        System.out.println(result);
+    }
 }
+
+
+
+
