d262dc24c1b3d47101c1d82faf9bf2db31f7fb87
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:28:28 +0530

    [Avinash] add. a program of in which we excuted the finally block each and every transtion over then finally block excuted

--- src/main/java/Day4ExceptionHandeling/finallyBlockExecution/DivisionClass.java
+++ src/main/java/Day4ExceptionHandeling/finallyBlockExecution/DivisionClass.java
@@ -1,4 +1,35 @@
 package Day4ExceptionHandeling.finallyBlockExecution;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class DivisionClass {
+    public static double divisbleNumber(double numerator, double denominator){
+        if(denominator==0){
+            throw new ArithmeticException( "Cannot divide by zero! ");
+        }
+        return numerator/denominator;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            System.out.println("Enter the numerator " );
+            double numerator= sc.nextDouble();
+
+            System.out.println("Enter the denominator: ");
+            double denominator = sc.nextDouble();
+
+            double result= divisbleNumber(numerator, denominator);
+            System.out.println("Result: "+ result);
+        }
+        catch(ArithmeticException e){
+            System.out.println("Error" +e.getMessage());
+        }
+        finally {
+            System.out.println("Operation completed");
+        }
+        sc.close();
+    }
 }
