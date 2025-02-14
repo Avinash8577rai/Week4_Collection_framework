81e134b749b9c936707795e70aff781b34962a81
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:38:03 +0530

    [Avinash] add. a program of test cases of the program of the exception propagetion

--- src/test/java/Day4ExceptionHandeling/ExceptionPropagation/ExceptionPropagationTest.java
+++ src/test/java/Day4ExceptionHandeling/ExceptionPropagation/ExceptionPropagationTest.java
@@ -1,4 +1,17 @@
 package Day4ExceptionHandeling.ExceptionPropagation;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class ExceptionPropagationTest {
+    @Test
+    void testExceptionPropagation(){
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        ExceptionPropagation.main(new String[]{});
+        String expectedOutput = "Handeled the exception in main";
+        assertEquals(expectedOutput, outContent.toString().trim());
+    }
 }
