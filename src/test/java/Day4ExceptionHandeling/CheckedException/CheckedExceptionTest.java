ae622fc6d04040da47330dce21303d83e5820461
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:36:58 +0530

    [Avinash] add. a program of test cases of the checked exception

--- src/test/java/Day4ExceptionHandeling/CheckedException/CheckedExceptionTest.java
+++ src/test/java/Day4ExceptionHandeling/CheckedException/CheckedExceptionTest.java
@@ -1,4 +1,26 @@
 package Day4ExceptionHandeling.CheckedException;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.io.FileWriter;
+import java.io.IOException;
 public class CheckedExceptionTest {
-}
+
+
+
+
+        @Test
+        void testFileExists() throws IOException {
+            String testFileName = "test_data.txt";
+            try (FileWriter writer = new FileWriter(testFileName)) {
+                writer.write("Hello, world!");
+            }
+
+            assertEquals("Hello, world!", CheckedException.readfiles(testFileName));
+        }
+
+        @Test
+        void testFileNotFound() {
+            assertEquals("Error file not found", CheckedException.readfiles ("nonexistent.txt"));
+        }
+    }
+
