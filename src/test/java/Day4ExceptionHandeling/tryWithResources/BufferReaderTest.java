9ab484095d8797739ce790d2be33373d8f2fe36d
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:42:18 +0530

    [Avinash] add. a program of test cases of the program of buffer reader of file and shows exception

--- src/test/java/Day4ExceptionHandeling/tryWithResources/BufferReaderTest.java
+++ src/test/java/Day4ExceptionHandeling/tryWithResources/BufferReaderTest.java
@@ -1,4 +1,15 @@
 package Day4ExceptionHandeling.tryWithResources;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 
 public class BufferReaderTest {
-}
+    @Test
+    void testFileRead() throws IOException {
+        String result = BufferReader.readFiles("non_existent_file.txt");
+        assertNull(result);
+    }
+
+    }
+
