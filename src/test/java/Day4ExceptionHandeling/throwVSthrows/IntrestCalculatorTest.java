a7f18f4adf5b84a43672dbc5ef25db4847f134c3
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:41:26 +0530

    [Avinash] add. a program of test cases of the program of throw and throws and use exception

--- src/test/java/Day4ExceptionHandeling/throwVSthrows/IntrestCalculatorTest.java
+++ src/test/java/Day4ExceptionHandeling/throwVSthrows/IntrestCalculatorTest.java
@@ -1,4 +1,25 @@
 package Day4ExceptionHandeling.throwVSthrows;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class IntrestCalculatorTest {
+    @Test
+    void testValidIntrestCalculator(){
+        double interest=IntrestCalculator.intrestCalculator(1000,5,2);
+        assertEquals(100,interest, 0.001);
+    }
+    @Test
+    void testNegativeAmmount(){
+        Exception exception= assertThrows(IllegalArgumentException.class, ()->{
+            IntrestCalculator.intrestCalculator(-1000,5,2);
+        });
+                assertEquals("Amount and Rate must be in Positive ",exception.getMessage());
+    }
+    @Test
+    void testNegativeRate(){
+        Exception exception= assertThrows(IllegalArgumentException.class, ()->{
+            IntrestCalculator.intrestCalculator(1000,-5,2);
+        });
+        assertEquals("Amount and Rate must be in Positive ",exception.getMessage());
+    }
+
 }
