3785fe5f2ef2f5ba9082d60dfdceb09d10f3f736
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:27:38 +0530

    [Avinash] add. a program of in which we use funtion to call in other function to raise the excepation  and handle the exception in main

--- src/main/java/Day4ExceptionHandeling/ExceptionPropagation/ExceptionPropagation.java
+++ src/main/java/Day4ExceptionHandeling/ExceptionPropagation/ExceptionPropagation.java
@@ -1,4 +1,19 @@
 package Day4ExceptionHandeling.ExceptionPropagation;
 
 public class ExceptionPropagation {
+    public static void method1(){
+        int result = 10/0;
+    }
+    public static void method2(){
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try{
+            method2();
+        }
+        catch(ArithmeticException e){
+            System.out.println("Handeled the exception in main");
+        }
+    }
 }
