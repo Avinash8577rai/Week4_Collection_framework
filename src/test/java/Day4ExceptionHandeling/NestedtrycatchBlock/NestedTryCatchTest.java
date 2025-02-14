afc31f9b828aae91b8db83ea785b6cd1855e424e
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-14 15:40:01 +0530

    [Avinash] add. a program of test cases of the program of the nested try and catch exception

--- src/test/java/Day4ExceptionHandeling/NestedtrycatchBlock/NestedTryCatchTest.java
+++ src/test/java/Day4ExceptionHandeling/NestedtrycatchBlock/NestedTryCatchTest.java
@@ -1,4 +1,42 @@
 package Day4ExceptionHandeling.NestedtrycatchBlock;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class NestedTryCatchTest {
+
+    @Test
+    void testValidDivison(){
+        ByteArrayOutputStream outContent= new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        int[] arr={10,20,30,40,50};
+        NestedTryCatch.divideArray(arr,2,5);
+        String  expectedoutput="Result:6";
+        assertEquals(expectedoutput, outContent.toString().trim());
+
+    }
+    @Test
+    void testInvaildIndex(){
+        ByteArrayOutputStream outContent= new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        int[] arr={10,20,30,40,50};
+        NestedTryCatch.divideArray(arr,10,5);
+        String expectedoutput="Invalid! array index";
+        assertEquals(expectedoutput, outContent.toString().trim());
+    }
+    @Test
+    void testInvalidDivison(){
+        ByteArrayOutputStream outContent= new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        int[] arr={10,20,30,40,50};
+        NestedTryCatch.divideArray(arr, 2,0);
+        String expectedoutput="Cannot devide by zero!";
+        assertEquals(expectedoutput,outContent.toString().trim());
+    }
 }
