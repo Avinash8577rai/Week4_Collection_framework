763239cf5a94cadfa7fabc44307855743501c6d2
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:36:57 +0530

    [Avinash] add. a program of licence plate number using regex

--- src/main/java/Day5/Regex/BasicRegexProblems/ValidateLicensePlateNumber/LicensePlate.java
+++ src/main/java/Day5/Regex/BasicRegexProblems/ValidateLicensePlateNumber/LicensePlate.java
@@ -1,4 +1,8 @@
 package Day5.Regex.BasicRegexProblems.ValidateLicensePlateNumber;
-
+import java.util.regex.*;
 public class LicensePlate {
+    public static final Pattern numberplate=Pattern.compile("^[A-Z]{2}[0-9]{4}");
+    public boolean isNumberPlate(String number){
+        return number!=null && numberplate.matcher(number).matches();
+    }
 }
