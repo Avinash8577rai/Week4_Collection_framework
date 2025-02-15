866a24ade94c6892049de7230509293a3822ac12
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:39:52 +0530

    [Avinash] add. a program of extracting cpital words from string using regex

--- src/main/java/Day5/Regex/ExtractionProblems/ExtractCapitalizedWords/CaptialWordExtract.java
+++ src/main/java/Day5/Regex/ExtractionProblems/ExtractCapitalizedWords/CaptialWordExtract.java
@@ -1,4 +1,24 @@
 package Day5.Regex.ExtractionProblems.ExtractCapitalizedWords;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CaptialWordExtract {
+    private static final Pattern captialWords= Pattern.compile("");
+
+    public List<String>list(String words){
+        List<String>captializedWords= new ArrayList<>();
+        if(words==null|| words.isEmpty()) return captializedWords;
+
+        Matcher matcher= captialWords.matcher(words);
+        while (matcher.find()){
+            captializedWords.add(matcher.group());
+        }
+        return captializedWords;
+
+    }
+
+
 }
