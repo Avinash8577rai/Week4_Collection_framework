f1203d282004fbc7ce4ab368e3bb1651ef6ab204
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:46:28 +0530

    [Avinash] add. a program of testCase to check the test of user registration program

--- src/test/java/Day5/JUnit/Advance/TestingUserRegistration/UserRegistrationTest.java
+++ src/test/java/Day5/JUnit/Advance/TestingUserRegistration/UserRegistrationTest.java
@@ -1,4 +1,19 @@
 package Day5.JUnit.Advance.TestingUserRegistration;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class UserRegistrationTest {
+     private UserRegistration userRegistration= new UserRegistration();
+
+     @Test
+    void testValidUser(){
+         assertEquals("User Registered Successfully", userRegistration.registerUser("Avinash Kumar Rai", "avinashkumarrai8577@gmail.com","Avinash8577"));
+         assertEquals("User Registered Successfully", userRegistration.registerUser("Anand Soni", "anand@gmail.com","Anand8577"));
+     }
+     @Test
+    void testInvaildUser(){
+         assertThrows(IllegalArgumentException.class, () -> userRegistration.registerUser("", "avinashkumarrai8577@gamil.com", "ValidPass1"));
+         assertThrows(IllegalArgumentException.class, () -> userRegistration.registerUser("Raj", "RAJ@GAMIL", "Valid"));
+
+         assertThrows(IllegalArgumentException.class, () -> userRegistration.registerUser("Raj", "avinashkumarrai8577@gmail.com", "Valid"));
+     }
 }
