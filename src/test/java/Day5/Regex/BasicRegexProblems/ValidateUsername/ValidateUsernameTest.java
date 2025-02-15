a5435676ef1c4e57b686ac5b6823750d20e8bb52
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:56:37 +0530

    [Avinash] add. a program of testCase to check the test of program of validing the username using regex

--- src/test/java/Day5/Regex/BasicRegexProblems/ValidateUsername/ValidateUsernameTest.java
+++ src/test/java/Day5/Regex/BasicRegexProblems/ValidateUsername/ValidateUsernameTest.java
@@ -1,4 +1,18 @@
 package Day5.Regex.BasicRegexProblems.ValidateUsername;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class ValidateUsernameTest {
+    private final ValidateUsername validateUsername = new ValidateUsername();
+
+    @Test
+    void testValidUsername(){
+        assertTrue(validateUsername.isValidUsername("Avinash8577"));
+        assertTrue(validateUsername.isValidUsername("raiSaheb_8577"));
+    }
+    @Test
+    void testInvaildUsername(){
+        assertFalse(validateUsername.isValidUsername(null));
+        assertFalse(validateUsername.isValidUsername("longer_than_15_chars"));
+        assertFalse(validateUsername.isValidUsername("use"));
+    }
 }
