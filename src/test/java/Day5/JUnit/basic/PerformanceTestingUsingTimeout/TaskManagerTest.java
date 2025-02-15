10e70663f3b5170f0cedcd7048896ea408c8fd27
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:47:35 +0530

    [Avinash] add. a program of testCase to check the test of task manager program

--- src/test/java/Day5/JUnit/basic/PerformanceTestingUsingTimeout/TaskManagerTest.java
+++ src/test/java/Day5/JUnit/basic/PerformanceTestingUsingTimeout/TaskManagerTest.java
@@ -1,4 +1,17 @@
 package Day5.JUnit.basic.PerformanceTestingUsingTimeout;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.util.concurrent.TimeUnit;
 
 public class TaskManagerTest {
+
+    private final TaskManager taskManager = new TaskManager();
+
+    @Test
+    @Timeout(value = 3, unit = TimeUnit.SECONDS)
+    void testLongRunning(){
+        taskManager.logRunningTask();
+    }
 }
