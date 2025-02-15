0fbe50ffb22f04465b21f5a76d665dd3c463ee7a
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:32:55 +0530

    [Avinash] add. a program of finding reapting words from a string using regex

--- src/main/java/Day5/Regex/AdvancedProblems/FindRepeatingWords/RepeatingWordsFinder.java
+++ src/main/java/Day5/Regex/AdvancedProblems/FindRepeatingWords/RepeatingWordsFinder.java
@@ -1,4 +1,26 @@
 package Day5.Regex.AdvancedProblems.FindRepeatingWords;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class RepeatingWordsFinder {
+    public static Set<String> findRepeatingWords(String text) {
+        Set<String> seenWords = new HashSet<>();
+        Set<String> repeatedWords = new HashSet<>();
+
+        if (text == null || text.isEmpty()) return repeatedWords;
+
+
+        Pattern pattern = Pattern.compile("\\b\\w+\\b", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text.toLowerCase());
+
+        while (matcher.find()) {
+            String word = matcher.group();
+            if (!seenWords.add(word)) {
+                repeatedWords.add(word);
+            }
+        }
+        return repeatedWords;
+    }
 }
+
