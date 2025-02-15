4c727e80ad8ffa4faaa8932b58fcdd64036e66b5
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:37:27 +0530

    [Avinash] add. a program of username valid using regex

--- src/main/java/Day5/Regex/BasicRegexProblems/ValidateUsername/ValidateUsername.java
+++ src/main/java/Day5/Regex/BasicRegexProblems/ValidateUsername/ValidateUsername.java
@@ -1,4 +1,10 @@
 package Day5.Regex.BasicRegexProblems.ValidateUsername;
 
+import java.util.regex.Pattern;
+
 public class ValidateUsername {
+    private static final Pattern username= Pattern.compile("^[a-zA-Z0-9_]{4,14}$");
+    public boolean isValidUsername(String usernames){
+        return usernames!= null && username.matcher(usernames).matches();
+    }
 }
