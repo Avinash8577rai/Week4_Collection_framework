100f19a841aa087f0d3c0af404083853edfd18c4
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:59:56 +0530

    [Avinash] add. a program of testCase to check the test of program of extracting the email Address from the string using regex

--- src/test/java/Day5/Regex/ExtractionProblems/ExtractEmailAddresses/EmailExtractTest.java
+++ src/test/java/Day5/Regex/ExtractionProblems/ExtractEmailAddresses/EmailExtractTest.java
@@ -1,4 +1,25 @@
 package Day5.Regex.ExtractionProblems.ExtractEmailAddresses;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class EmailExtractTest {
+    private final EmailExtract emailExtract = new EmailExtract();
+
+    @Test
+    void testExtractedEmails(){
+        String  text= "Contact us on avinashkumarrai8577@gmail.com and support@technook.edu";
+        List<String>emails=emailExtract.extractedEmail(text);
+        assertEquals(2,emails.size());
+        assertTrue(emails.contains("avinashkumarrai8577@gmail.com"));
+        assertTrue(emails.contains("support@technook.edu"));
+    }
+    @Test
+    void testNoEmails(){
+        String text="Anand bnajdbhasfjhdvjbcvhdcghjcg";
+        List<String>emails=emailExtract.extractedEmail(text);
+        assertTrue(emails.isEmpty());
+    }
+
 }
