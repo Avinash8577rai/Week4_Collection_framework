ce47a4344e40d3587700fd2b27384050cf0bbfd8
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:54:29 +0530

    [Avinash] add. a program of testCase to check the test of program of validing the IP address

--- src/test/java/Day5/Regex/AdvancedProblems/ValidateIPAddress/IpAddressTest.java
+++ src/test/java/Day5/Regex/AdvancedProblems/ValidateIPAddress/IpAddressTest.java
@@ -1,4 +1,28 @@
 package Day5.Regex.AdvancedProblems.ValidateIPAddress;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class IpAddressTest {
-}
+
+        @Test
+        void testValidIPv4() {
+            assertTrue(IpAddress.isValidIPv4("192.168.1.1"));
+            assertTrue(IpAddress.isValidIPv4("255.255.255.255"));
+            assertTrue(IpAddress.isValidIPv4("0.0.0.0"));
+        }
+
+        @Test
+        void testInvalidIPv4() {
+            assertFalse(IpAddress.isValidIPv4("256.100.100.100"));
+            assertFalse(IpAddress.isValidIPv4("192.168.1"));
+
+        }
+
+        @Test
+        void testNullAndEmpty() {
+            assertFalse(IpAddress.isValidIPv4(null));
+            assertFalse(IpAddress.isValidIPv4(""));
+        }
+    }
+
+
