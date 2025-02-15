1240b7f7a5126e5b594738573954d0ee6bb85a17
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:42:03 +0530

    [Avinash] add. a program of replacing the space in the string using regex

--- src/main/java/Day5/Regex/ExtractionProblems/ReplaceSpace/ReplaceSpace.java
+++ src/main/java/Day5/Regex/ExtractionProblems/ReplaceSpace/ReplaceSpace.java
@@ -1,4 +1,10 @@
 package Day5.Regex.ExtractionProblems.ReplaceSpace;
 
 public class ReplaceSpace {
-}
+
+        public static String normalizeSpaces(String text) {
+            if (text == null) return null; // Handle null case
+            return text.replaceAll("\\s+", " ").trim(); // Replace multiple spaces with a single space
+        }
+    }
+
