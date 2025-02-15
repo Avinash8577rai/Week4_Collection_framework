2c2c890b7b5fd04185586447f32b979d6b93cd84
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 21:01:00 +0530

    [Avinash] add. a program of testCase to check the test of program of replacing the space from the string using regex

--- src/test/java/Day5/Regex/ExtractionProblems/ReplaceSpace/ReplaceSpaceTest.java
+++ src/test/java/Day5/Regex/ExtractionProblems/ReplaceSpace/ReplaceSpaceTest.java
@@ -1,4 +1,31 @@
 package Day5.Regex.ExtractionProblems.ReplaceSpace;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class ReplaceSpaceTest {
-}
+
+        @Test
+        void testMultipleSpaces() {
+            assertEquals("This is an example with multiple spaces.",
+                    ReplaceSpace.normalizeSpaces("This    is  an  example   with   multiple    spaces."));
+        }
+
+        @Test
+        void testLeadingTrailingSpaces() {
+            assertEquals("Hello world!", ReplaceSpace.normalizeSpaces("   Hello    world!   "));
+        }
+
+        @Test
+        void testSingleWord() {
+            assertEquals("Java", ReplaceSpace.normalizeSpaces("    Java    "));
+        }
+
+        @Test
+        void testNoExtraSpaces() {
+            assertEquals("No extra spaces here", ReplaceSpace.normalizeSpaces("No extra spaces here"));
+        }
+
+
+
+    }
+
+
