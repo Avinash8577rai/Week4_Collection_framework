5b075ce3e5d18102748eb6aafe52f5e3d6492673
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:26:02 +0530

    [Avinash] add. a program of calculator and if excpetion come handle it and  test it using junit

--- src/main/java/Day5/JUnit/basic/TestingExceptionHandling/Calculator.java
+++ src/main/java/Day5/JUnit/basic/TestingExceptionHandling/Calculator.java
@@ -1,4 +1,11 @@
 package Day5.JUnit.basic.TestingExceptionHandling;
 
 public class Calculator {
+    public int divide(int a, int b){
+        if(b==0){
+            throw new ArithmeticException("Division by Zero not Allowed");
+        }
+        return a/b;
+    }
+
 }
