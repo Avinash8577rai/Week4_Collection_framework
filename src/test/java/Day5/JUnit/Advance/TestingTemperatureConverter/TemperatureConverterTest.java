cc8678c882969a787fdf9fa2d81e389e4ac231a3
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:45:59 +0530

    [Avinash] add. a program of testCase to check the test of temerpature conversion program

--- src/test/java/Day5/JUnit/Advance/TestingTemperatureConverter/TemperatureConverterTest.java
+++ src/test/java/Day5/JUnit/Advance/TestingTemperatureConverter/TemperatureConverterTest.java
@@ -1,4 +1,18 @@
 package Day5.JUnit.Advance.TestingTemperatureConverter;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class TemperatureConverterTest {
+    private TemperatureConverter temperatureConverter = new TemperatureConverter();
+
+    @Test
+    void testCelsiusConverter(){
+        assertEquals(32.0, temperatureConverter.celsiusToFreinheit(0),0.01);
+        assertEquals(98.6, temperatureConverter.celsiusToFreinheit(37), 0.01);
+    }
+    @Test
+    void testFreinheitConverter(){
+        assertEquals(37, temperatureConverter.freinheitToCelsius(98.6),0.01);
+        assertEquals(100.0, temperatureConverter.freinheitToCelsius(212), 0.01);
+    }
+
 }
