e7f2259d129b9a45ff852237d4a86442d949c077
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:34:27 +0530

    [Avinash] add. a program of making and validing the ip adress using regex

--- src/main/java/Day5/Regex/AdvancedProblems/ValidateIPAddress/IpAddress.java
+++ src/main/java/Day5/Regex/AdvancedProblems/ValidateIPAddress/IpAddress.java
@@ -1,4 +1,18 @@
 package Day5.Regex.AdvancedProblems.ValidateIPAddress;
-
+import java.util.regex.Pattern;
 public class IpAddress {
-}
+
+        private static final String IPaddress =
+                "^(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\." +
+                        "(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\." +
+                        "(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)\\." +
+                        "(25[0-5]|2[0-4][0-9]|1?[0-9][0-9]?)$";
+
+        private static final Pattern pattern = Pattern.compile(IPaddress);
+
+        public static boolean isValidIPv4(String ip) {
+            if (ip == null) return false; // Null check
+            return pattern.matcher(ip).matches();
+        }
+    }
+
