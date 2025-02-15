2850424f38df035a5d1fc91136fd3d41dfee6d3d
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:28:47 +0530

    [Avinash] add. a program of string utlity and test it using junit

--- src/main/java/Day5/JUnit/basic/TestingStringMethods/StringUtils.java
+++ src/main/java/Day5/JUnit/basic/TestingStringMethods/StringUtils.java
@@ -1,4 +1,24 @@
 package Day5.JUnit.basic.TestingStringMethods;
 
 public class StringUtils {
+    public String reverse(String str){
+        if(str==null){
+            return null;
+        }
+        return new StringBuilder(str).reverse().toString();
+    }
+    public boolean isPalindrom(String str){
+        if(str==null){
+            return false;
+        }
+        String reversed= reverse(str);
+        return str.equalsIgnoreCase(reversed);
+    }
+    public String toUpperCase(String str){
+        if(str==null){
+            return null;
+
+        }
+        return str.toUpperCase();
+    }
 }
