df9c1dd5c6adcc1c44055c70ccc04e0de84e0c71
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:57:58 +0530

    [Avinash] add. a program of testCase to check the test of program of extracting the bad words from trhe string using regex

--- src/test/java/Day5/Regex/ExtractionProblems/CensorBadWords/BadWordsTest.java
+++ src/test/java/Day5/Regex/ExtractionProblems/CensorBadWords/BadWordsTest.java
@@ -1,4 +1,27 @@
 package Day5.Regex.ExtractionProblems.CensorBadWords;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class BadWordsTest {
-}
+
+
+
+        @Test
+        void testCensorBadWords() {
+            assertEquals("This is a **** bad example with some **** words.",
+                    BadWords.censorBadWords("This is a damn bad example with some stupid words."));
+        }
+
+
+        @Test
+        void testNoBadWords() {
+            assertEquals("This is a clean sentence.", BadWords.censorBadWords("This is a clean sentence."));
+        }
+
+
+
+        @Test
+        void testNullInput() {
+            assertNull(BadWords.censorBadWords(null));
+        }
+    }
+
