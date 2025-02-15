61cdd08656e0bed44848a75243c27d45097e4d4e
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:36:20 +0530

    [Avinash] add. a program of hex color using regex

--- src/main/java/Day5/Regex/BasicRegexProblems/ValidateHexColorCode/HexColorCode.java
+++ src/main/java/Day5/Regex/BasicRegexProblems/ValidateHexColorCode/HexColorCode.java
@@ -1,4 +1,9 @@
 package Day5.Regex.BasicRegexProblems.ValidateHexColorCode;
-
+import java.util.regex.*;
 public class HexColorCode {
+
+    public static final Pattern hexcolorcode= Pattern.compile("^#{1}[0-9A-Fa-f]{6}");
+    public boolean isHexCode(String code){
+        return code!=null&&  hexcolorcode.matcher(code).matches();
+    }
 }
