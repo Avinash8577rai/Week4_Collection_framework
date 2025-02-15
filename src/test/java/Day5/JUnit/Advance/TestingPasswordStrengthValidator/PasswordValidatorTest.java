d7567ae9a0552a5ea2de1ddd2203c4d98a34a4a7
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:45:30 +0530

    [Avinash] add. a program of testCase to check the test of password validtor program

--- src/test/java/Day5/JUnit/Advance/TestingPasswordStrengthValidator/PasswordValidatorTest.java
+++ src/test/java/Day5/JUnit/Advance/TestingPasswordStrengthValidator/PasswordValidatorTest.java
@@ -1,4 +1,20 @@
 package Day5.JUnit.Advance.TestingPasswordStrengthValidator;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class PasswordValidatorTest {
+    private final PasswordValidator passwordValidator= new PasswordValidator();
+
+    @Test
+    void testPasswordIsValid(){
+        assertTrue(passwordValidator.isValid("StrongPass1"));
+        assertTrue(passwordValidator.isValid("Hello123"));
+    }
+    @Test
+    void testInvaildPassword(){
+        assertFalse(passwordValidator.isValid(null));
+        assertFalse(passwordValidator.isValid("ahsbjbcc"));
+        assertFalse(passwordValidator.isValid("bvsah"));
+
+    }
+
 }
