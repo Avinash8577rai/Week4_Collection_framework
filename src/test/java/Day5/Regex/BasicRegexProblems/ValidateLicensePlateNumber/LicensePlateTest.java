8fafeeeb8b29fc6703e54fef6a867fd3af8a6354
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:56:10 +0530

    [Avinash] add. a program of testCase to check the test of program of validing the licence plate number using regex

--- src/test/java/Day5/Regex/BasicRegexProblems/ValidateLicensePlateNumber/LicensePlateTest.java
+++ src/test/java/Day5/Regex/BasicRegexProblems/ValidateLicensePlateNumber/LicensePlateTest.java
@@ -1,4 +1,19 @@
 package Day5.Regex.BasicRegexProblems.ValidateLicensePlateNumber;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class LicensePlateTest {
+    private final LicensePlate licensePlate=new LicensePlate();
+
+    @Test
+    void testvalidNumberPlate(){
+        assertTrue(licensePlate.isNumberPlate("AB8577"));
+        assertTrue(licensePlate.isNumberPlate("AK5396"));
+
+    }
+    @Test
+    void testInvalidNumberPlate(){
+        assertFalse(licensePlate.isNumberPlate("AVI4525"));
+        assertFalse(licensePlate.isNumberPlate("av8577"));
+    }
+
 }
