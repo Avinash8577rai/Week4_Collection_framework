7bcef82e6f4f01047a6ae3e8d48c41467cc66129
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:19:39 +0530

    [Avinash] add. a program of testing date format useing junit

--- src/main/java/Day5/JUnit/Advance/TestingDateFormatte/DateFormatter.java
+++ src/main/java/Day5/JUnit/Advance/TestingDateFormatte/DateFormatter.java
@@ -1,4 +1,17 @@
 package Day5.JUnit.Advance.TestingDateFormatte;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DateFormatter {
+    public String formatDate(String inputDate){
+         try{
+             LocalDate date= LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+             return date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+         }
+         catch(DateTimeParseException e){
+             throw new IllegalArgumentException("Invalid date format. expected yyyy-MM-dd.");
+         }
+    }
+
 }
