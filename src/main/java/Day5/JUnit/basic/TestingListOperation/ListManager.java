a6fe5abce6347dc4611a41d4bcf73f804660d074
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:27:26 +0530

    [Avinash] add. a program of list operation  and test it using junit

--- src/main/java/Day5/JUnit/basic/TestingListOperation/ListManager.java
+++ src/main/java/Day5/JUnit/basic/TestingListOperation/ListManager.java
@@ -1,4 +1,22 @@
 package Day5.JUnit.basic.TestingListOperation;
 
+import java.util.List;
+
 public class ListManager {
+    public void addElement(List<Integer>list,int element){
+        if(list!=null){
+            list.add(element);
+        }
+
+    }
+    public boolean removeElement(List<Integer>list, int element){
+        if(list!=null){
+            return list.remove(Integer.valueOf(element));
+        }
+        return false;
+    }
+    public int getsize(List<Integer>list){
+        return (list!=null)? list.size():0;
+    }
 }
+
