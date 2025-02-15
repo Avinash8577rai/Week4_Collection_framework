3dd7585a9dabf02b27a44fbf0674e6da8f907b73
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:54:56 +0530

    [Avinash] add. a program of testCase to check the test of program of validing the SSN

--- src/test/java/Day5/Regex/AdvancedProblems/ValidateSSN/SSNValidatorTest.java
+++ src/test/java/Day5/Regex/AdvancedProblems/ValidateSSN/SSNValidatorTest.java
@@ -1,4 +1,20 @@
 package Day5.Regex.AdvancedProblems.ValidateSSN;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 public class SSNValidatorTest {
+    @Test
+    void testValidSSNs() {
+        assertTrue(SSNValidator.isValidSSN("123-45-6789"));
+        assertTrue(SSNValidator.isValidSSN("987-65-4321"));
+    }
+
+    @Test
+    void testInvalidSSNs() {
+        assertFalse(SSNValidator.isValidSSN("123456789"));
+        assertFalse(SSNValidator.isValidSSN("123-456-789"));
+
+    }
 }
+
