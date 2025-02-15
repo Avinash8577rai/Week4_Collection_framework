c20f4324d35d51a8e1158d55b4f9ff43118d21d5
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:31:23 +0530

    [Avinash] add. a program of currency exctract using regex

--- src/main/java/Day5/Regex/AdvancedProblems/ExtractCurrencyValues/CurrencyExtractor.java
+++ src/main/java/Day5/Regex/AdvancedProblems/ExtractCurrencyValues/CurrencyExtractor.java
@@ -1,4 +1,20 @@
 package Day5.Regex.AdvancedProblems.ExtractCurrencyValues;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class CurrencyExtractor {
+    private static final Pattern curreny = Pattern.compile("\\$?\\d+(\\.\\d{2})?");
+
+    public static List<String> extractCurrencyValues(String text) {
+        List<String> currencyValues = new ArrayList<>();
+        if (text == null) return currencyValues; // Null check
+
+        Matcher matcher = curreny.matcher(text);
+        while (matcher.find()) {
+            currencyValues.add(matcher.group());
+        }
+        return currencyValues;
+    }
 }
+
