131b37fee63cf7c8055b3aa56ac79e62604be7cd
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:40:56 +0530

    [Avinash] add. a program of extracting email adress from the string using regex

--- src/main/java/Day5/Regex/ExtractionProblems/ExtractEmailAddresses/EmailExtract.java
+++ src/main/java/Day5/Regex/ExtractionProblems/ExtractEmailAddresses/EmailExtract.java
@@ -1,4 +1,17 @@
 package Day5.Regex.ExtractionProblems.ExtractEmailAddresses;
-
+import java.util.regex.*;
+import java.util.*;
 public class EmailExtract {
+    private static final Pattern emailExtracted= Pattern.compile("[a-z0-9_]+@[a-z0-9]+\\.[a-z0-9]{2,}");
+    public List<String>extractedEmail(String text){
+        List<String>emails= new ArrayList<>();
+        if(text==null)return emails;
+        Matcher matcher= emailExtracted.matcher(text);
+        while (matcher.find()){
+            emails.add(matcher.group());
+        }
+        return emails;
+
+    }
+
 }
