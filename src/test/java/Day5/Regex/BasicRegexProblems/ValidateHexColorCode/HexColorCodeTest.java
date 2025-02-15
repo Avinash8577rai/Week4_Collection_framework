141d1db34a9b773fb8d208d1fbd961a8d9046ecf
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:55:49 +0530

    [Avinash] add. a program of testCase to check the test of program of validing the hexa color code using regex

--- src/test/java/Day5/Regex/BasicRegexProblems/ValidateHexColorCode/HexColorCodeTest.java
+++ src/test/java/Day5/Regex/BasicRegexProblems/ValidateHexColorCode/HexColorCodeTest.java
@@ -1,4 +1,17 @@
 package Day5.Regex.BasicRegexProblems.ValidateHexColorCode;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class HexColorCodeTest {
+    private final HexColorCode hexColorCode= new HexColorCode();
+
+    void testvalidHexColorCode(){
+        assertTrue(hexColorCode.isHexCode("#AB8577"));
+        assertTrue(hexColorCode.isHexCode("#BH5396"));
+
+    }
+    @Test
+    void testInvalidHexColorCode(){
+        assertFalse(hexColorCode.isHexCode("AVI4525"));
+        assertFalse(hexColorCode.isHexCode("av8577"));
+    }
 }
