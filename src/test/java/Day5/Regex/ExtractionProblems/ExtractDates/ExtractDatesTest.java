b15632e301e330d99467d2cf54cb3cd11b6abf00
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:59:33 +0530

    [Avinash] add. a program of testCase to check the test of program of extracting the dates from the string using regex

--- src/test/java/Day5/Regex/ExtractionProblems/ExtractDates/ExtractDatesTest.java
+++ src/test/java/Day5/Regex/ExtractionProblems/ExtractDates/ExtractDatesTest.java
@@ -1,4 +1,33 @@
 package Day5.Regex.ExtractionProblems.ExtractDates;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class ExtractDatesTest {
+    private static final ExtractDates extract= new ExtractDates();
+
+    @Test
+    void testExtractDates() {
+        String text = "The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.";
+        List<String> words = extract.extractdates(text);
+
+        assertEquals(7, words.size());
+        assertTrue(words.contains("12/05/2023"));
+        assertTrue(words.contains("15/08/2024"));
+        assertTrue(words.contains("29/02/2020"));
+
+    }
+
+    @Test
+    void testNoDates() {
+        String text = "this is a simple sentence with no capital letters.";
+        List<String> words = extract.extractdates(text);
+        assertTrue(words.isEmpty());
+    }
+
+
 }
