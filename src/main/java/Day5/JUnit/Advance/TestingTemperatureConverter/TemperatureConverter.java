0803dfae421bdba96e126742c9ffc671a656bbab
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-02-15 20:20:50 +0530

    [Avinash] add. a program of testting temperature conversion using junit

--- src/main/java/Day5/JUnit/Advance/TestingTemperatureConverter/TemperatureConverter.java
+++ src/main/java/Day5/JUnit/Advance/TestingTemperatureConverter/TemperatureConverter.java
@@ -1,4 +1,12 @@
 package Day5.JUnit.Advance.TestingTemperatureConverter;
 
 public class TemperatureConverter {
+    public double celsiusToFreinheit(double celsius){
+        return (celsius*9/5)+32;
+    }
+    public double freinheitToCelsius(double freinheit){
+        return (freinheit-32)*5/9;
+
+    }
+
 }
